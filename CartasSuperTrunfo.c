9708357f3e508e2f4cd55116a727e94fdaa83fb4
Jefferson Ismael <144837101+jeffersonismael@users.noreply.github.com>
2025-09-08 14:12:53 +0000

    nivel-aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,11 +2,11 @@
 
 int main(){
 
-    printf("Desafio Super Trunfo - Nível Novato\n");
+    printf("Desafio Super Trunfo - Nível Aventureiro\n");
 
     char estado1[1], estado2[1], cidade1[8], cidade2[8];
     int populacao1, populacao2, pontosTuristicos1, pontosTuristicos2;
-    float area1, area2, pib1, pib2;
+    float area1, area2, pib1, pib2, densidade1, densidade2, pibPerCapita1, pibPerCapita2;
 
     printf("Carta 1:\nDigite o Estado: \n");
     scanf("%s", &estado1);
@@ -26,7 +26,10 @@ int main(){
     printf("Digite o PIB: \n");
     scanf("%f", &pib1);
 
-    printf("Carta 1:\nEstado: %s\nCódigo: %s01\nNome da Cidade: %s\nPopulação: %d milhões\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado1, estado1, cidade1, populacao1, area1, pib1, pontosTuristicos1);
+    densidade1 = populacao1 / area1;
+    pibPerCapita1 = pib1 / populacao1;
+
+    printf("Carta 1:\nEstado: %s\nCódigo: %s01\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n", estado1, estado1, cidade1, populacao1, area1, pib1, pontosTuristicos1, densidade1, pibPerCapita1);
 
     printf("Carta 2:\nDigite o Estado: \n");
     scanf("%s", &estado2);
@@ -46,7 +49,10 @@ int main(){
     printf("Digite o PIB: \n");
     scanf("%f", &pib2);
 
-    printf("Carta 2:\nEstado: %s\nCódigo: %s02\nNome da Cidade: %s\nPopulação: %d milhões\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado2, estado2, cidade2, populacao2, area2, pib2, pontosTuristicos2);
+    densidade2 = populacao2 / area2;
+    pibPerCapita2 = pib2 / populacao2;
+
+    printf("Carta 2:\nEstado: %s\nCódigo: %s02\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n", estado2, estado2, cidade2, populacao2, area2, pib2, pontosTuristicos2, densidade2, pibPerCapita2);
 
     return 0;
 }
